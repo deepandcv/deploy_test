1af09ef429564d976728c1e9745880947b0be841
deepandcv <149863618+deepandcv@users.noreply.github.com>
2023-12-01 15:34:22 +0530

    Add files via upload

---
+++ deploy_test.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from PIL import Image
+
+st.title('Uber pickups in NYC')
+
+st.header('This is a header with a divider', divider='rainbow')
+st.header('_Streamlit_ is :blue[cool] :sunglasses:')
+
+st.caption('This is a string that explains something above.')
+st.caption('A caption with _italics_ :blue[colors] and emojis :sunglasses:')
